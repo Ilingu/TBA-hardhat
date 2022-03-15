d723b263e9d8ba4193ee84a4653acc9a32c5b1fc
Ilingu <thetitouoff@gmail.com>
2022-03-15 22:14:19 +0100

    [BETA.2]: Contract V2 + Test

--- contracts/BestAnime.sol
+++ contracts/BestAnime.sol
@@ -3,13 +3,18 @@ pragma solidity ^0.8.0;
 
 // [ADDRESS]: 0xFF738C6988155096F9D912daB32D9C9eBB120B1C
 
+/* Add Oracle (Don't Work Well Yet: For Future Version)
+    - [https://docs.witnet.io/smart-contracts/apis-and-http-get-post-oracle/make-a-get-request]
+    - Make A req to Jikan to see if anime exist, if not: revert
+*/
+
 contract BestAnimeContract {
   address private owner;
 
   string public BestAnime;
 
-  address OwnerOfBestAnime;
-  uint256 LastBuyPrice;
+  address public OwnerOfBestAnime;
+  uint256 public LastBuyPrice;
 
   constructor(string memory _initAnime) {
     owner = msg.sender;
@@ -22,8 +27,19 @@ contract BestAnimeContract {
 
   fallback() external payable {}
 
+  event NewBestAnime(
+    string NewBestAnime,
+    uint256 NewBuyPrice,
+    address NewOwner
+  );
+
   modifier onlyOwner() {
     require(msg.sender == owner, "Not The Owner :)");
+    _;
+  }
+
+  modifier NotOwner() {
+    require(msg.sender != owner, "You're the Onwer :)");
     _;
   }
 
@@ -47,11 +63,13 @@ contract BestAnimeContract {
     return (BestAnime, LastBuyPrice, OwnerOfBestAnime);
   }
 
-  function setBestAnime(string memory _bestanime) external payable {
+  function setBestAnime(string memory _bestanime) external payable NotOwner {
     require(msg.value > LastBuyPrice, "You have to pay above the last price");
+
     LastBuyPrice = msg.value;
     OwnerOfBestAnime = msg.sender;
 
     BestAnime = _bestanime;
+    emit NewBestAnime(BestAnime, LastBuyPrice, OwnerOfBestAnime);
   }
 }
--- test/test.js
+++ test/test.js
@@ -1,21 +1,65 @@
 const { expect } = require("chai");
+const { BigNumber } = require("ethers");
+const { parseEther, formatEther } = require("ethers/lib/utils");
 const { ethers } = require("hardhat");
 
 let Contract, contract;
+let BEST_ANIME = "Sasaki To Miyano";
+
+// formatEther(1000000000000000000n) --> wei to eth
 
 describe("BestAnime", function () {
   beforeEach(async () => {
     Contract = await ethers.getContractFactory("BestAnimeContract");
-    contract = await Contract.deploy();
+    contract = await Contract.deploy(BEST_ANIME);
     await contract.deployed();
   });
 
   it("Should return the best anime in the world", async function () {
-    const BEST_ANIME = "Sasaki To Miyano";
+    // Getter
+    const GetBestAnime = async () =>
+      (await contract.TheBestAnime()).toString().split(",")[0];
+    const GetOwnerBA = async () =>
+      (await contract.OwnerOfBestAnime()).toString().split(",")[0];
+    const GetLastBuyPrice = async () =>
+      parseInt((await contract.LastBuyPrice()).toString());
+    const GetBalance = async () =>
+      parseInt((await contract.getSCBalance()).toString());
 
-    const setAnimeTx = await contract.setBestAnime(BEST_ANIME);
+    let TheCurrentBestAnime = await GetBestAnime();
+    let LastBuyPrice = await GetLastBuyPrice();
+    let OwnerBA = await GetOwnerBA();
+    let Balance = await GetBalance();
+
+    // Accounts
+    const [owner, addr1 /*, addr2, ... */] = await ethers.getSigners();
+
+    // return console.log();
+
+    // Test
+    expect(LastBuyPrice).to.equal(0);
+    expect(Balance).to.equal(0);
+    expect(TheCurrentBestAnime).to.equal(BEST_ANIME);
+    expect(OwnerBA).to.equal(owner.address);
+    BEST_ANIME = "Bungo Stray Dogs";
+
+    // Set New Price To +1 ETH
+    const valToPay = BigInt(LastBuyPrice) + 1000000000000000000n;
+    // Contract Initiate With Owner, So If We do the Tx With Owner --> Revert
+    // .connect() change the address of the sender
+    const setAnimeTx = await contract.connect(addr1).setBestAnime(BEST_ANIME, {
+      value: valToPay,
+    });
     await setAnimeTx.wait();
 
-    expect(await contract.TheBestAnime()).to.equal(BEST_ANIME);
+    TheCurrentBestAnime = await GetBestAnime();
+    LastBuyPrice = await GetLastBuyPrice();
+    OwnerBA = await GetOwnerBA();
+    Balance = await GetBalance();
+
+    expect(BigInt(LastBuyPrice)).to.equal(valToPay);
+    expect(Balance).to.equal(LastBuyPrice);
+    expect(TheCurrentBestAnime).to.equal(BEST_ANIME);
+    expect(OwnerBA).to.equal(addr1.address);
   });
 });
